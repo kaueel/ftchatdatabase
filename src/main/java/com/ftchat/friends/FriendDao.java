1ad97e6cad09e14695d1feec89ec97dfd14c03c7
Felipe Gomiero <f171257@g.unicamp.br>
2017-10-16 20:15:19 -0200

    criando classe Dao

--- src/main/java/com/ftchat/friends/FriendDao.java
+++ src/main/java/com/ftchat/friends/FriendDao.java
@@ -3,7 +3,7 @@ package com.ftchat.friend;
 import java.util.List;
 
 public interface FriendDao {
-    public List<Friend> getAllFriends() throws Exception;
+    public List<> getAllFriends() throws Exception;
     public Friend addFriend(int user_primary, int user_secondary) throws Exception;
     public Friend verifyFriend
     public Friend verifyFriend(int user_primary, int user_secondary) throws Exception;
