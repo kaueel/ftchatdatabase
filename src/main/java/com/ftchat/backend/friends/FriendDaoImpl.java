0997a2a1f7db768bf517a4f6ad47a7edd0e679bd
Felipe Gomiero <f171257@g.unicamp.br>
2017-10-16 20:27:47 -0200

    criando classe dao e implementacao

---
+++ src/main/java/com/ftchat/backend/friends/FriendDaoImpl.java
@@ -0,0 +1,11 @@
+package com.ftchat.friend;
+
+import com.ftchat.dao.DaoOwner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class FriendDaoImpl extends DaoOwner{
+
+}
